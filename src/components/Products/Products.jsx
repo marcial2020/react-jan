c5ebc685d1490a601f7ed62168e89ba714b3ebff
marcial melong <s1270701@student.mcckc.edu>
2021-01-15 12:58:38 -0600

    add my first commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products =[
+    {
+    id: 0,
+    name: "iPhone 11 - Red",
+    featured: true,
+    img: "assets/images/products/iPhone/iphone1.jpeg",
+    price: 599,
+    company: "Apple",
+    info:
+      "Released 2019, The iPhone 11 succeeds the iPhone XR, and it features a 6.1-inch LCD display that Apple calls a Liquid Retina HD Display. It features a 1792 x 828 resolution at 326ppi, a 1400:1 contrast ratio, 625 nits max brightness, True Tone support for adjusting the white balance to the ambient lighting, and wide color support for true-to-life colors.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 1,
+    title: "iPhone 11 - Black",
+    featured: false,
+    img: "assets/images/products/iPhone/iphone4.jpeg",
+    price: 599,
+    company: "Apple",
+    info:
+      "Released 2019, The iPhone 11 succeeds the iPhone XR, and it features a 6.1-inch LCD display that Apple calls a Liquid Retina HD Display. It features a 1792 x 828 resolution at 326ppi, a 1400:1 contrast ratio, 625 nits max brightness, True Tone support for adjusting the white balance to the ambient lighting, and wide color support for true-to-life colors.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "iPhone 11 - White",
+    featured: false,
+    img: "assets/images/products/iPhone/iphone3.jpeg",
+    price: 599,
+    company: "Apple",
+    info:
+      "Released 2019, The iPhone 11 succeeds the iPhone XR, and it features a 6.1-inch LCD display that Apple calls a Liquid Retina HD Display. It features a 1792 x 828 resolution at 326ppi, a 1400:1 contrast ratio, 625 nits max brightness, True Tone support for adjusting the white balance to the ambient lighting, and wide color support for true-to-life colors.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: "iPhone 11 - Green",
+    featured: false,
+    img: "assets/images/products/iPhone/iphone5.jpeg",
+    price: 599,
+    company: "Apple",
+    info:
+      "Released 2019, The iPhone 11 succeeds the iPhone XR, and it features a 6.1-inch LCD display that Apple calls a Liquid Retina HD Display. It features a 1792 x 828 resolution at 326ppi, a 1400:1 contrast ratio, 625 nits max brightness, True Tone support for adjusting the white balance to the ambient lighting, and wide color support for true-to-life colors.",
+    inCart: false,
+    count: 0,
+    total: 0,
+  }
+]
+
+const Products = () => {
+
+    return (
+
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                <Grid item key={product.id} xs={12} md={6} lg={3}>
+                    <Product  product={product} />
+                </Grid>
+                ))}
+            </Grid>
+        </main>
+    )
+    
+}
+
+export default Products;
